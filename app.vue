ebeeb52c9a57765ff9b0aec54deaccd013e59cb2
Jorge DC <jddelcampom@gmail.com>
2024-03-28 22:42:51 -0500

    start the project by adding the initial configurations

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url';
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url)),
+  },
+  vite: {
+    css:{
+      preprocessorOptions:{
+        scss: {
+          additionalData: `
+            @import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+            @import '@fortawesome/fontawesome-free/scss/solid.scss';
+            @import '@fortawesome/fontawesome-free/scss/regular.scss';
+            @import '@fortawesome/fontawesome-free/scss/brands.scss';
+            @import '@/styles/main.scss';
+            @import '@nabux-crush/crush-styles/nabuxLayoutVariables';
+          `
+        }
+      }
+    }
+  }
+})
