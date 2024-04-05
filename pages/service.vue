aeeeda6780659fb0e1f67ceebdac6b385aa1db70
Jorge DC <jddelcampom@gmail.com>
2024-04-05 14:32:39 -0500

    Update meta tags

--- pages/service.vue
+++ pages/service.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: 'Cepol | Servicios',
+  meta: [
+    {
+      name: 'description',
+      content: 'Conoce los servicios que ofrecemos para tu éxito universitario. ¡Desbloquea tus habilidades con nuestros profesores y materiales de estudio excepcionales!'
+    }
+  ]
+})
+</script>
+
 <template>
 <Service />
 </template>
