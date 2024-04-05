63d8b39370251ae0923ac61dab09d9a4f08b63c6
Jorge DC <jddelcampom@gmail.com>
2024-04-05 14:32:50 -0500

    Update SectionCards component and content

--- components/SectionCards/index.vue
+++ components/SectionCards/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { content1, content2, content3 } from '@/utils/content/SectionCards/Content'
+import { content1, content2 } from '@/utils/content/SectionCards/Content'
 </script>
 
 <template>
@@ -11,6 +11,12 @@ import { content1, content2, content3 } from '@/utils/content/SectionCards/Conte
       :paragraph="content1.paragraph"
       :buttonContent="content1.buttonContent"
     />
+    <blockquote class="section__blockquote">
+      “La educación es el arma más poderosa para salvar el mundo”
+      <footer class="section__blockquote__footer">
+        - <cite class="footer--author">Nelson Mandela</cite>
+      </footer>
+    </blockquote>
     <SectionCardsModel
       :image="content2.image"
       :spanTitle="content2.spanTitle"
@@ -18,13 +24,6 @@ import { content1, content2, content3 } from '@/utils/content/SectionCards/Conte
       :paragraph="content2.paragraph"
       :buttonContent="content2.buttonContent"
       :reverseSection="true"
-    />
-    <SectionCardsModel
-      :image="content3.image"
-      :spanTitle="content3.spanTitle"
-      :title="content3.title"
-      :paragraph="content3.paragraph"
-      :buttonContent="content3.buttonContent"
     />
   </div>
 </template>
@@ -34,5 +33,27 @@ import { content1, content2, content3 } from '@/utils/content/SectionCards/Conte
   display: flex;
   flex-direction: column;
   gap: 80px;
+}
+.section__blockquote{
+  font-family: $secondary-font;
+  padding: 20px 0;
+  font-size: 1.5rem;
+  font-style: italic;
+  font-weight: $font-weight-bold;
+  text-align: center;
+  background-color: rgba(128, 128, 128, .2);
+  @media (min-width: $tablet-upper-breakpoint){
+    font-size: 2rem;
+    padding: 60px 40px;
+  }
+  &__footer{
+    font-size: 1rem;
+    font-weight: $font-weight-normal;
+    margin-top: 10px;
+    @media (min-width: $tablet-upper-breakpoint){
+      font-size: 1.25rem;
+      margin-top: 20px;
+    }
+  }
 }
 </style>
--- utils/content/SectionCards/Content.ts
+++ utils/content/SectionCards/Content.ts
@@ -1,6 +1,5 @@
 import IMAGE1 from '@/assets/section1.webp'
-import IMAGE2 from '@/assets/section2.webp'
-import IMAGE3 from '@/assets/section3.webp' 
+import IMAGE2 from '@/assets/section2.webp' 
 
 
 export const content1 = {
@@ -13,14 +12,6 @@ export const content1 = {
 
 export const content2 = {
   image: IMAGE2,
-  spanTitle: 'Estudiantes',
-  title: 'Tú puedes aprender lo que el futuro necesita.',
-  paragraph: 'Construye un entendimiento profundo y sólido en matemáticas, ciencias y más.',
-  buttonContent: 'Estudiantes, inicien aquí'
-}
-
-export const content3 = {
-  image: IMAGE3,
   spanTitle: 'Juntos podemos hacer la diferencia',
   title: 'Todos los jovenes merecen la oportunidad de aprender.',
   paragraph: 'Cada año en Ecuador, más de 100.000 jóvenes se quedan sin cupo para ingresar a la Educación Superior. Con CEPOL, tú puedes cambiar tu futuro',
